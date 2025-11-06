a179e82a1767d94f9ee591843402accc814ed966
Ryan Shino <ryanshino@yahoo.com>
2025-11-06 23:15:35 +0000

    Fixing difficulties and minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -29,9 +29,6 @@ export default function App() {
       setplayedMoves((prev) => [...prev, result.san])
   
       switch(difficulty) {
-        case "random":
-          setTimeout(() => makeRandomMove(newGame), 400);
-          break;
         case "easy":
           playAIMove(newGame, 1);
           break;
@@ -41,11 +38,8 @@ export default function App() {
         case "hard":
           playAIMove(newGame, 3);
           break;
-        case "extreme":
-          playAIMove(newGame, 4);
-          break;
         default:
-          setTimeout(() => makeRandomMove(newGame), 400);
+          playAIMove(newGame, 1);
       }
       return true;
     } else {
@@ -66,30 +60,11 @@ export default function App() {
     }
   }
 
-  const makeRandomMove = (currentGame) => {
-    const newGame = new Chess(currentGame.fen());
-    const moves = newGame.moves();
-
-    if (moves.length === 0) return;
-
-    const randomMove = moves[Math.floor(Math.random() * moves.length)];
-    const result = newGame.move(randomMove);
-
-    if (result) {
-      setGame(new Chess(newGame.fen()));
-
-      setplayedMoves((prev) => [...prev, result.san])
-    }
-  };
-
   return (
     <div className="chess-container">
       <h2>Choose your difficulty:</h2>
       <div className="board-wrapper"> 
         <div className='button-wrapper'>
-          <button onClick={() => 
-            setDifficulty('random')} 
-            className={difficulty === 'random' ? 'selected' : ''}>Random</button>
           <button onClick={() => 
             setDifficulty('easy')} 
             className={difficulty === 'easy' ? 'selected' : ''}>Easy</button>
@@ -99,9 +74,6 @@ export default function App() {
             <button onClick={() => 
             setDifficulty('hard')} 
             className={difficulty === 'hard' ? 'selected' : ''}>Hard</button>
-            <button onClick={() => 
-            setDifficulty('extreme')} 
-            className={difficulty === 'extreme' ? 'selected' : ''}>Extreme</button>
         </div>
         <div className="chess-board-wrapper">
           <Chessboard
